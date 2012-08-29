ceeaeff1863c1032e12c2ab9063296a2e35d7741
unknown <rlum@Vanint53.vancouver.polycom.com>
2012-08-29 11:50:50 -0700

    getaddrinfo(gethostname) with ai_passive : working
    initial commit.  Works and returns both ipv4 and ipv6 addresses on
    windows machine.   Based on code that worked on linux but this
    version not tested.  Windows error handling does work with
    wide char to char conversions

---
+++ getaddrinfo/getaddrinfo.cpp
@@ -0,0 +1,172 @@
+// getaddrinfo.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+#ifdef WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+//#include "stdafx.h"
+//#include <wspiapi.h>
+//#include <windows.h>
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h> // inet_ntop
+#include <unistd.h> // gethostname
+#endif
+
+
+#define HOSTNAMESZ 128
+#define VERBOSE 1
+#define DEBUG if (VERBOSE) printf
+
+#ifdef WIN32
+  WSADATA WSStartData;
+
+void initialize_sockets()
+{
+  int rc = WSAStartup(MAKEWORD (2,0),&WSStartData);
+  DEBUG("WSAStartData.szDescription = %s\n",WSStartData.szDescription);
+  DEBUG("WSAStartData.szSystemStatus = %s\n", WSStartData.szSystemStatus);
+  DEBUG("WSAStartData.iMaxSockets = %d\n", WSStartData.iMaxSockets);
+  switch (rc){
+    case 0:
+      //all ok
+      break;
+    case WSASYSNOTREADY:
+      printf("WSASYSNOTREADY: The underlying network subsystem is not ready for network communication.\n");
+      break;
+    case WSAVERNOTSUPPORTED:
+      printf("WSAVERNOTSUPPORTED: The version of Windows Sockets support requested is not provided by this particular Sockets implementation\n");
+      break;
+    case WSAEINPROGRESS:
+      printf("WSAEINPROGRESS: A blocking Windows Sockets 1.1 operation is in progress\n");
+      break;
+    case WSAEPROCLIM:
+      printf("WSAEPROCLIM: A limit on the number of tasks supported by the Windows Sockets implementation has been reached.\n");
+      break;
+    case WSAEFAULT:
+      printf("WSAEFAULT: The lpWSAData parameter is not a valid pointer.\n");
+      break;
+    default:
+      printf("WSAStartUp error code %d\n", rc);
+      break;
+  }
+}
+
+void cleanup_sockets()
+{
+    char running[] ="Running";
+    if (strncmp(running,WSStartData.szSystemStatus, strlen(running))==0){
+      DEBUG("WSA is running, cleaning up");
+      WSACleanup();
+    }else{
+      DEBUG("WSA is '%s' %d, no need to clean up",WSStartData.szSystemStatus,strlen(WSStartData.szSystemStatus));
+    }
+}
+
+wchar_t* wsaerrorstr(int errnumber){
+  DWORD dwFlags = FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS;
+  LPCVOID lpSource = NULL;
+  DWORD dwMessageId = errnumber;  // WSAGetLastError();
+  DWORD dwLanguageId = MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT);
+  LPWSTR lpBuffer=NULL;
+  DWORD nSize = 0;
+  va_list *Args = NULL;
+
+  DWORD buffsize  = FormatMessage( dwFlags, lpSource, dwMessageId, dwLanguageId,
+          (LPWSTR)&lpBuffer, nSize, Args);
+
+  if (buffsize == 0){
+    wchar_t* tmp = (wchar_t*)malloc(256);
+    wcsncpy (tmp,L"error code: ",sizeof(tmp));
+    wchar_t* num = (wchar_t*) malloc(256);
+    int size = _snwprintf(num,sizeof (num),L"%d",errnumber);
+    wcsncat(tmp,num,sizeof(tmp));
+    return tmp;
+  }else{
+    return  lpBuffer;
+  }
+}
+
+char*  wchar_to_char(wchar_t* orig,char* nstring){
+  size_t origsize = wcslen(orig)+1;
+  //const size_t newsize = 1000;
+  size_t convertedChars = 0;
+  //char nstring[newsize];
+  wcstombs_s(&convertedChars, nstring, origsize, orig, _TRUNCATE);
+  //strcat_s(nstring, " (char *)");
+  return nstring;
+}
+#endif
+
+const char* getErrorString(){
+#ifdef WIN32
+    int error = WSAGetLastError();
+    wchar_t *error_msg = wsaerrorstr(error);
+    char *errorstring = (char*)malloc(1000);
+    const char *errstring = wchar_to_char(error_msg,errorstring);
+    return errstring;
+#else
+   return (strerror(errno));
+#endif
+}
+
+int main(int argc, char** argv){
+  struct addrinfo   hints;
+  struct addrinfo * local_addr;
+  memset((char*)&hints, 0, sizeof(hints));
+  hints.ai_flags  = AI_PASSIVE;
+  //hints.ai_family = AF_INET6;
+  int rc; 
+
+  char hostname[HOSTNAMESZ];
+  memset(hostname,0,HOSTNAMESZ);
+  initialize_sockets();
+  if (argc==1){
+    if((rc=gethostname(hostname,HOSTNAMESZ))!=0){
+      printf("Failed to gethostname, error:%s",getErrorString());
+    }
+  }else{
+    strncpy(hostname, argv[1],sizeof(hostname));
+  }  
+
+  rc = getaddrinfo(hostname,
+                        NULL,
+                        &hints,
+                        &local_addr);
+
+  if (rc == 0){
+    int counter = 1;
+    addrinfo* next = local_addr;
+    do {
+      printf("\tai_flags = %d\n\tai_family = %d\n\tai_socktype = %d\n\tai_protocol = %d\n\tai_addrlen = %d\n\tai_canonname = %s\n\tai_addr = %x\n",
+        next->ai_flags,
+        next->ai_family,
+        next->ai_socktype,
+        next->ai_protocol,
+        next->ai_addrlen,
+        next->ai_canonname,
+        (int)next->ai_addr);
+        char ipaddr[INET6_ADDRSTRLEN];
+        //(struct sockaddr_in *)next->ai_addr;
+        if (next->ai_family == AF_INET)
+          inet_ntop(next->ai_family, (void*)&((struct sockaddr_in*)next->ai_addr)->sin_addr, ipaddr, sizeof(ipaddr));
+        else if (next->ai_family == AF_INET6)
+          inet_ntop(next->ai_family, (void*)(&((struct sockaddr_in6*)next->ai_addr)->sin6_addr), ipaddr, sizeof(ipaddr));
+        else
+          printf ("unrecognized address famiy %d\n", next->ai_family);
+        printf("address %d = %s\n", counter++, ipaddr);
+        next = next->ai_next;
+    }while(next);
+  }else{
+    printf ("getaddrinfo failed\n");
+  }
+
+  cleanup_sockets();
+}
+
